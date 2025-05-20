30666c75a2d41e68a84bb69b0d99b26181780c97
Aldemir Junior <159361439+AldemirJunior1@users.noreply.github.com>
2025-05-20 17:22:08 -0300

    Update mockData.js

--- data/mockData.js
+++ data/mockData.js
@@ -1,11 +1,13 @@
 const netflixTop10 = [
   { title: "Stranger Things", img: "https://m.media-amazon.com/images/I/91miBpFYN5L.jpg", description: "Mistério e ficção científica." },
   { title: "The Witcher", img: "https://images.justwatch.com/poster/258546181/s718/the-witcher.jpg", description: "Aventura e fantasia." },
-  // ... 8 outros
+  { title: "Nonnas", img: "https://cinemaeafins.com.br/trilhas/wp-content/uploads/2025/05/trilha-sonora-de-nonnas-2025.webp", description: "Comédia dramática sobre um restaurante comandado por avós italianas." },
+  { title: "Paddington in Peru", img: "https://www.sonypictures.ca/sites/canada/files/2025-03/DP_9586683_PADDINGTONINPERU_2000x3000_HEKeyArt_1333x2000_thumbnail-min.jpg", description: "O adorável urso embarca em uma aventura na selva peruana." },
+  //Favor colocar os outros 6 conforme o exemplo acima ass:jr. OBS: não esqueça as virgulas. 
 ];
 
 const primeTop10 = [
   { title: "The Boys", img: "https://m.media-amazon.com/images/S/pv-target-images/473fd8bc878799c1a035cb13c688edd9eb6d240d426abf34e0bf3c1dde95724b.jpg", description: "Super-heróis com um toque sombrio." },
-  { title: "Jack Ryan", img: "https://m.media-amazon.com/images/S/pv-target-images/6b8e2acc37990972190b6e9d0486e421fd69796665b7b66d89f38ebfd6f26589.jpg", description: "Suspense político." },
+  { title: "Jack Ryan", img: "https://upload.wikimedia.org/wikipedia/pt/b/b6/Jack_Ryan_s%C3%A9rie.jpg", description: "Suspense político." },
   // ... 8 outros
 ];
